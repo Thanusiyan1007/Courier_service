74ceeaf570213e565406f8c4d4f7bda9914e1eb9
Rayappu Thanusiyan <157779212+Thanusiyan1007@users.noreply.github.com>
2024-12-11 20:57:56 +0530

    modify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     return (
         <div className='absolute top-0 left-0 w-full z-10'>
             <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
-                <img src={logo} alt="logo" className='h-24' />
+                <img src={logo} alt="logo" className='h-8' />
                 <ul className='hidden md:flex gap-7 text-white'>
                     <a href="#Home" className='cursor-pointer hover:text-gray-400'>Home</a>
                     <a href="#About" className='cursor-pointer hover:text-gray-400'>About</a>
